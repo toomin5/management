60940314b07ad03977689e49aa96da4468881b66
lee minho <twomean99@gmail.com>
2023-08-21 20:16:09 +0900

    정보

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: "1",
+    image: "https://placeimg.com/64/64/any",
+    name: "이민호",
+    birthday: "990409",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: "2",
+    image: "https://placeimg.com/64/64/any",
+    name: "김민수",
+    birthday: "960409",
+    gender: "남자",
+    job: "회사원",
+  },
+  {
+    id: "3",
+    image: "https://placeimg.com/64/64/any",
+    name: "주현영",
+    birthday: "980309",
+    gender: "여자",
+    job: "대학생",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <h2>let'go</h2>
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
